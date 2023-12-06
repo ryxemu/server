675dedb40cf43444efa0b54f97e10917dd18091c
Rynet <109999485+Constantine-Decrypt@users.noreply.github.com>
2023-12-05 21:54:35 -0500

    network packet error

--- common/emu_tcp_connection.cpp
+++ common/emu_tcp_connection.cpp
@@ -434,44 +434,45 @@ void EmuTCPConnection::ClearBuffers() {
 }
 
 void EmuTCPConnection::SendNetErrorPacket(const char* reason) {
-#if TCPC_DEBUG >= 1
-	struct in_addr in;
-	in.s_addr = GetrIP();
-	std::cout << "NetError: '";
-	if (reason)
-		std::cout << reason;
-	std::cout << "': " << inet_ntoa(in) << ":" << GetPort() << std::endl;
-#endif
-	
-	auto pack = new ServerPacket(0);
-	pack->size = 1;
+    #if TCPC_DEBUG >= 1
+        struct in_addr in;
+        in.s_addr = GetrIP();
+        std::cout << "NetError: '";
+        if (reason)
+            std::cout << reason;
+        std::cout << "': " << inet_ntoa(in) << ":" << GetPort() << std::endl;
+    #endif
 
-	if (reason != nullptr)
-	pack->size += strlen(reason) + 1;
+    auto pack = new ServerPacket(0);
+    pack->size = 1;
 
-	// Allocate memory for pBuffer
-	pack->pBuffer = new uchar[pack->size];
-	memset(pack->pBuffer, 0, pack->size);
-	pack->pBuffer[0] = 255;
+    if (reason != nullptr)
+        pack->size += strlen(reason) + 1;
 
-	if (reason != nullptr) {
-	// Check if there's enough space for the reason
-	size_t remainingSpace = pack->size - 1;
-	size_t reasonLength = strlen(reason);
-	if (reasonLength < remainingSpace) {
-	strcpy((char*)&pack->pBuffer[1], reason);
-	} else {
-	strncpy((char*)&pack->pBuffer[1], reason, remainingSpace - 1);
-	// Ensure null termination
-	pack->pBuffer[pack->size - 1] = '\0';
-	}
-	} else {
-	strcpy((char*)&pack->pBuffer[1], "unknown");
-	}
+    // Allocate memory for pBuffer
+    pack->pBuffer = new uchar[pack->size];
+    memset(pack->pBuffer, 0, pack->size);
+    pack->pBuffer[0] = 255;
 
-	SendPacket(pack);
-	safe_delete(pack);
+    if (reason != nullptr) {
+        size_t remainingSpace = pack->size - 1;
+        size_t reasonLength = strlen(reason);
+
+        if (reasonLength < remainingSpace) {
+            strncpy((char*)&pack->pBuffer[1], reason, remainingSpace - 1);
+            pack->pBuffer[pack->size - 1] = '\0'; // Ensure null termination
+        } else {
+            strncpy((char*)&pack->pBuffer[1], reason, remainingSpace - 1);
+            pack->pBuffer[pack->size - 1] = '\0'; // Ensure null termination
         }
+    } else {
+        strncpy((char*)&pack->pBuffer[1], "unknown", pack->size - 1);
+        pack->pBuffer[pack->size - 1] = '\0'; // Ensure null termination
+    }
+
+    SendPacket(pack);
+    safe_delete(pack);
+}
 
 void EmuTCPConnection::RemoveRelay(EmuTCPConnection* relay, bool iSendRelayDisconnect) {
 	if (iSendRelayDisconnect) {
